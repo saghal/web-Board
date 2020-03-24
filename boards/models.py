46e555bcd9d7546a50f0b9c5efcec7bf5d7c2e0e
saghal <mohammadsgh98@gmail.com>
2020-03-25 01:50:34 +0430

    add post model and migrate

--- boards/models.py
+++ boards/models.py
@@ -4,7 +4,17 @@ from django.contrib.auth.models import User
 class Board(models.Model):
     name = models.CharField(max_length = 30, unique = True)
     description = models.CharField(max_length = 100)
+
 class Topic(models.Model):
     subject = models.CharField(max_length = 255)
-    last_updated = models.DateTimeField(auto_now = True)
-    board = models.ForeingKey(Board,on_delete=CASCADE)
+    last_updated = models.DateTimeField(auto_now_add = True)
+    board = models.ForeignKey(Board, on_delete = models.PROTECT, related_name='topics')
+    starter = models.ForeignKey(User, on_delete = models.PROTECT, related_name='topics')
+
+class Post(models.Model):
+    message = models.TextField(max_length = 4000)
+    topic = models.ForeignKey(Topic, on_delete = models.PROTECT, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(null = True)
+    created_by = models.ForeignKey(User, on_delete = models.PROTECT, related_name='posts')
+    updated_by = models.ForeignKey(User, null = True, on_delete = models.PROTECT, related_name='+')
