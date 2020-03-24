bda1686803dcf637415106b943bb413e28e454fd
saghal <mohammadsgh98@gmail.com>
2020-03-24 21:47:49 +0430

    add board and topic in model

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length = 30, unique = True)
+    description = models.CharField(max_length = 100)
+class Topic(models.Model):
+    subject = models.CharField(max_length = 255)
+    last_updated = models.DateTimeField(auto_now = True)
+    board = models.ForeingKey(Board,on_delete=CASCADE)
