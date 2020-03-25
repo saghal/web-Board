a777200e7e45fb75ae31d7caf3b9528501b1e1e1
saghal <mohammadsgh98@gmail.com>
2020-03-25 05:30:01 +0430

    add new topic template

--- webBoard/urls.py
+++ webBoard/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
 
 
 ]
