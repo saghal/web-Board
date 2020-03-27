4c32b2efaac64be39bc4165dfb2f448b13b5f818
saghal <mohammadsgh98@gmail.com>
2020-03-28 03:48:41 +0430

    add login template

--- webBoard/settings.py
+++ webBoard/settings.py
@@ -126,3 +126,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
--- webBoard/urls.py
+++ webBoard/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('signup/',accounts_views.signup, name = 'signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+
 
 ]
