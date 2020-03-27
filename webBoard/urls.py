4e810c3daa0703bd13b1dcab090c82cd24d3c2ce
saghal <mohammadsgh98@gmail.com>
2020-03-28 03:39:44 +0430

    add logout and add dropdown to navbar

--- webBoard/urls.py
+++ webBoard/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from boards import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +25,6 @@ urlpatterns = [
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('signup/',accounts_views.signup, name = 'signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ]
